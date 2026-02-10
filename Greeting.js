46ccbd17b225e0cb0e609072acb2429451c138ae
AKahbeah <albenson@mail.fhsu.edu>
2026-02-09 23:06:27 -0600

    Assignment 1

---
+++ Greeting.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Greeting() {
+  const today = new Date().toDateString();
+
+  const paragraphStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+
+  return (
+    <div>
+      <h1>Hello, Welcome to React!</h1>
+      <p style={paragraphStyle}>
+        Today’s date is: {today}
+      </p>
+    </div>
+  );
+}
+
+export default Greeting;
